f8ff51b4c4d27f8ef9e09c384f6fe969c96fc319
REIGaruki <gyakujo@mail.ru>
2024-12-10 02:43:52 +0300

    Изменены интерфейсы

--- src/main/java/ru/skypro/homework/service/AdsMapper.java
+++ src/main/java/ru/skypro/homework/service/AdsMapper.java
@@ -1,10 +1,12 @@
 package ru.skypro.homework.service;
 
+import org.mapstruct.Mapper;
 import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.dto.Ads;
 
 import java.util.List;
 
+@Mapper
 public interface AdsMapper {
     Ads mapToAds(List<Ad> ads);
 }
--- src/main/java/ru/skypro/homework/service/ExtendedAdMapper.java
+++ src/main/java/ru/skypro/homework/service/ExtendedAdMapper.java
@@ -1,8 +1,12 @@
 package ru.skypro.homework.service;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.dto.ExtendedAd;
+import ru.skypro.homework.dto.User;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ExtendedAdMapper {
-    ExtendedAd mapToExtendedAd(Ad ad);
+    ExtendedAd mapToExtendedAd(User user, Ad ad);
 }
