04fd31190d298283ffa50e191357566a89d8f2a2
HomeMedia <taknado13@yandex.ru>
2024-12-25 16:11:37 +0700

    исправлен сервис под мапперы(в работе)

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -1,10 +1,14 @@
+
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.model.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findByAdId(Integer adId);
+    List<Comment> findById(int id);
+    Comment findByAdIdAndId(int adId, Integer commentId);
 }
+
