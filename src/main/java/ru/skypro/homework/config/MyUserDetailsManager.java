56d8598076ba4b80c07af73d40b85531c2adb748
REIGaruki <gyakujo@mail.ru>
2024-12-27 20:51:41 +0300

    Changes2

--- src/main/java/ru/skypro/homework/config/MyUserDetailsManager.java
+++ src/main/java/ru/skypro/homework/config/MyUserDetailsManager.java
@@ -10,28 +10,51 @@ import ru.skypro.homework.exception.UserNotFoundException;
 import ru.skypro.homework.model.User;
 import ru.skypro.homework.repository.UserRepository;
 
+import java.util.Optional;
+
+/**
+ * Сервис для управления данными пользователей в Spring Security.
+ * Реализует интерфейс {@link UserDetailsService} для загрузки данных пользователя по email.
+ * Также предоставляет методы для создания новых пользователей и проверки их существования.
+ */
 @Service
 public class MyUserDetailsManager implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Конструктор для создания экземпляра MyUserDetailsManager.
+     *
+     * @param userRepository  Репозиторий для работы с сущностью {@link User}.
+     * @param passwordEncoder Кодировщик паролей для шифрования паролей пользователей.
+     */
     public MyUserDetailsManager(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-
+    /**
+     * Загружает данные пользователя по его email.
+     *
+     * @param phone Email пользователя.
+     * @return Объект {@link UserDetails}, содержащий данные пользователя.
+     * @throws UsernameNotFoundException Если пользователь с указанным email не найден.
+     */
     @Override
     public UserDetails loadUserByUsername(String phone) throws UsernameNotFoundException {
-        User user = userRepository.findByPhone(phone);
-        if (user == null) {
+        Optional<User> user = userRepository.findByEmail(phone);
+        if (user.isEmpty()) {
             throw new UserNotFoundException(phone);
         }
-        return new MyUserDetails(user);
+        return new MyUserDetails(user.get());
     }
 
+    /**
+     * Создает нового пользователя на основе данных из {@link UserDetails}.
+     *
+     * @param userDetails Объект {@link UserDetails}, содержащий данные нового пользователя.
+     */
     public void createUser(UserDetails userDetails) {
-        // Преобразуем UserDetails в сущность User
         User user = new User();
         user.setEmail(userDetails.getUsername());
         user.setPassword(passwordEncoder.encode(userDetails.getPassword()));
@@ -39,8 +62,13 @@ public class MyUserDetailsManager implements UserDetailsService {
         userRepository.save(user);
     }
 
+    /**
+     * Проверяет, существует ли пользователь с указанным email.
+     *
+     * @param username Email пользователя.
+     * @return {@code true}, если пользователь существует, иначе {@code false}.
+     */
     public boolean userExists(String username) {
-        // Проверка, существует ли пользователь
         return userRepository.findByEmail(username).isPresent();
     }
 }
--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -3,15 +3,27 @@ package ru.skypro.homework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.skypro.homework.dto.Role;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Конфигурационный класс для настройки Spring Security.
+ * Определяет правила безопасности, такие как аутентификация, авторизация и CORS,
+ * а также настраивает кодировщик паролей и пользователей в памяти.
+ */
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
+    /**
+     * Список URL-адресов, которые доступны без аутентификации.
+     */
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
             "/swagger-ui.html",
@@ -21,29 +33,57 @@ public class WebSecurityConfig {
             "/register"
     };
 
-
-
+    /**
+     * Создает менеджер пользователей в памяти с одним предопределенным пользователем.
+     *
+     * @param passwordEncoder Кодировщик паролей для шифрования пароля пользователя.
+     * @return {@link InMemoryUserDetailsManager} с предопределенным пользователем.
+     */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
-                .anyRequest().authenticated()
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user =
+                User.builder()
+                        .username("user@gmail.com")
+                        .password("password")
+                        .passwordEncoder(passwordEncoder::encode)
+                        .roles(Role.USER.name())
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /**
+     * Настраивает цепочку фильтров безопасности.
+     * Отключает CSRF, разрешает доступ к URL-адресам из белого списка без аутентификации,
+     * требует аутентификации для доступа к "/ads/**" и "/users/**", а также включает CORS и HTTP Basic аутентификацию.
+     *
+     * @param http Объект {@link HttpSecurity} для настройки безопасности.
+     * @return Сконфигурированная цепочка фильтров безопасности.
+     * @throws Exception Если возникает ошибка при настройке.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests(
+                        authorization ->
+                                authorization
+                                        .mvcMatchers(AUTH_WHITELIST)
+                                        .permitAll()
+                                        .mvcMatchers("/ads/**", "/users/**")
+                                        .authenticated())
+                .cors()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .httpBasic(withDefaults());
         return http.build();
     }
 
+    /**
+     * Создает и возвращает кодировщик паролей BCrypt.
+     *
+     * @return {@link BCryptPasswordEncoder} для шифрования паролей.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
